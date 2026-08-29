9a1c232b00f7207bda57806bdac122556d66a27e
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2026-08-29 00:09:10 -0500

    Nameplates: so, everything useful is a secret values so I reduced the logic to ALL casts

--- Nameplates.lua
+++ Nameplates.lua
@@ -1,41 +1,34 @@
 setfenv(1, _G.SlackHacks)
 
-local function isInterruptibleEnemyCast(unitTarget)
-	if not unitTarget or not UnitCanAttack("player", unitTarget) then
+local function isEnemyUnit(unitTarget)
+	if type(unitTarget) ~= "string" then
 		return false
 	end
 
-	local castName, castText, castTexture, castStartTime, castEndTime, isTradeSkill, castID, notInterruptibleCast, castSpellID = UnitCastingInfo(unitTarget)
-	if notInterruptibleCast ~= nil then
-		return not notInterruptibleCast
-	end
-
-	local channelName, channelText, channelTexture, channelStartTime, channelEndTime, channelIsTradeSkill, channelCastID, notInterruptableChannel, channelSpellID = UnitChannelInfo(unitTarget)
-	if notInterruptableChannel ~= nil then
-		return not notInterruptableChannel
-	end
-
-	return false
+	return UnitCanAttack("player", unitTarget)
 end
 
 function raiseCastingNameplate(unitTarget)
-	if not isInterruptibleEnemyCast(unitTarget) then
+	if not isEnemyUnit(unitTarget) then
 		return
 	end
 
-	log("Interruptible enemy cast detected: " .. tostring(unitTarget))
+	log("Enemy cast detected: " .. tostring(unitTarget))
 	local nameplate = C_NamePlate.GetNamePlateForUnit(unitTarget)
 	if nameplate then
-		nameplate:SetFrameLevel(9999)
-		log("Popped nameplate!")
+		local currentLevel = nameplate:GetFrameLevel()
+		nameplate:SetFrameLevel(currentLevel + 30)
+		log("Raised nameplate by 30 levels")
 	else
 		log("No nameplate found for unit: " .. tostring(unitTarget))
 	end
 end
 
-function handleInterruptibleCast(eventName, unitTarget)
-	if isInterruptibleEnemyCast(unitTarget) then
-		raiseCastingNameplate(unitTarget)
+function handleCasts(addonSelf, eventName, unitTarget)
+	if not isEnemyUnit(unitTarget) then
+		return
 	end
+
+	raiseCastingNameplate(unitTarget)
 end
 
