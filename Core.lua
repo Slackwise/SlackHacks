9d2912a1bef73d63a758107d272a8e3528cd11f5
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2026-09-07 22:22:57 -0500

    Max Camera Zoom: now unsets when you disable it back to default.

--- Core.lua
+++ Core.lua
@@ -280,6 +280,8 @@ end
 function setCVars()
   if db.profile.general.maximumCameraZoom then
     ensureCVar("cameraDistanceMaxZoomFactor", 2.6) -- Max out camera zoon
+  else
+    ensureCVar("cameraDistanceMaxZoomFactor", GetCVarDefault("cameraDistanceMaxZoomFactor"))
   end
   ensureCVar("minimapTrackingShowAll", 1) -- Show all minimap tracking options (including turning off target tracking!)
 
