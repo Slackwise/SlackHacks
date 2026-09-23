4dcb635843b3998d9dd53099d31f6c319cd0d3dc
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2026-09-22 21:12:19 -0500

    Profiles: added standard Ace3 Profiles!

--- Init.lua
+++ Init.lua
@@ -294,7 +294,10 @@ end
 
 --Event Handlers
 function Self:OnInitialize()
-  Self.db = LibStub("AceDB-3.0"):New("SlackHacksDB", dbDefaults)
+  -- true = share one "Default" profile across all characters instead of a per-character profile
+  Self.db = LibStub("AceDB-3.0"):New("SlackHacksDB", dbDefaults, true)
+  options.args.profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(Self.db)
+  options.args.profiles.order = 5.5
   migrateConfig()
   local customConfig = CustomConfigs and CustomConfigs[getBattletag()]
   if customConfig and customConfig.setOptions then
